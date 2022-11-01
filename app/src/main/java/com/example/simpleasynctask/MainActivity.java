5936e11d5bbd13a39e5b257ac75705b7dfb0580d
Panagiotis Efopoulos <panagiotis@tessera.gr>
2022-11-01 11:44:51 +0200

    challenge-1

--- app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -8,7 +8,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextView;
+    private TextView mTextView1;
+
     private static final String TEXT_STATE = "currentText";
 
     @Override
@@ -16,21 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = findViewById(R.id.textView1);
+        mTextView1 = findViewById(R.id.textView1);
 
         if (savedInstanceState != null) {
-            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+            mTextView1.setText(savedInstanceState.getString(TEXT_STATE));
         }
     }
 
     public void startTask(View view) {
-        mTextView.setText(R.string.napping);
-        new SimpleAsyncTask(mTextView).execute();
+        new SimpleAsyncTask(mTextView1).execute();
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(TEXT_STATE, mTextView.getText().toString());
+        outState.putString(TEXT_STATE, mTextView1.getText().toString());
     }
 }
--- app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
+++ app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
@@ -6,11 +6,11 @@ import android.widget.TextView;
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
-    private WeakReference<TextView> mTextView;
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
+    private WeakReference<TextView> mTextView1;
 
-    public SimpleAsyncTask(TextView tv) {
-        mTextView = new WeakReference<>(tv);
+    public SimpleAsyncTask(TextView tv1) {
+        mTextView1 = new WeakReference<>(tv1);
     }
 
     @Override
@@ -18,17 +18,33 @@ public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
         Random r = new Random();
         int n = r.nextInt(11);
         int s = n * 200;
+        for(int i = 0 ; i <= s ; i+=100) {
+            publishProgress(i);
+            try {
+                Thread.sleep(s/100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         try {
-            Thread.sleep(s);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         return "Awake at last after sleeping for " + s + " milliseconds!";
     }
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
-        mTextView.get().setText(s);
+        mTextView1.get().setText(s);
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        String val = Integer.toString(values[0]);
+        mTextView1.get().setText(val);
     }
 }
